4d8abb8437fdce420d91c9a42ab6b7d721603fe3
arielcondori <154715547+arielcondori@users.noreply.github.com>
2023-12-24 11:49:14 -0400

    Add files via upload

---
+++ login.js
@@ -0,0 +1,30 @@
+function loguear()
+{
+    let user=document.getElementById("usuario").value;
+    let pass=document.getElementById("clave").value;
+    if(user=="ratoncita" && pass=="14042023") {
+        window.location="hola.html";        
+    }
+    else{
+        alert("datos incorrectos");
+    }
+}
+
+function principal()
+{
+
+        window.location="principal.html";        
+
+}
+function imagenes()
+{
+
+        window.location="hola.html";        
+
+}
+function videos()
+{
+
+        window.location="videos.html";        
+
+}
